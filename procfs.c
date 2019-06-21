ca2e2414cc42ff24524e6909250261698f18bfdc
Shlomi <shlomi9987@gmail.com>
2019-06-21 15:05:06 +0300

    sprintf and sprinti

--- procfs.c
+++ procfs.c
@@ -25,6 +25,70 @@ enum my_inum {PROC, IDEINFO, FILESTAT, INODEINFO};
 struct superblock block;
 int blockInit = 0;
 
+static void
+putc(char* buff, char c, int *index)
+{
+  buff[(*index)++] = c;
+}
+
+static void
+sprinti(char *buff, int x, int *index)
+{
+  char buf[16];
+  int i = 0;
+  char digits[] = "0123456789";
+
+  do{
+    buf[i++] = digits[x % 10];
+  } while((x /= 10) != 0);
+
+  while(--i >= 0)
+    putc(buff, buf[i], index);
+}
+
+void
+sprintf(char* buff, char *fmt, ...)
+{
+  char *s;
+  int c, i, state;
+  uint *ap;
+  int index = 0;
+
+  state = 0;
+  ap = (uint*)(void*)&fmt + 1;
+  for(i = 0; fmt[i]; i++){
+    c = fmt[i] & 0xff;
+    if(state == 0){
+      if(c == '%'){
+        state = '%';
+      } else {
+        putc(buff, c, &index);
+      }
+    } else if(state == '%'){
+      if(c == 'd'){
+        sprinti(buff, *ap, &index);
+        ap++;
+      } else if(c == 's'){
+        s = (char*)*ap;
+        ap++;
+        if(s == 0)
+          s = "(null)";
+        while(*s != 0){
+          putc(buff, *s, &index);
+          s++;
+        }
+      } else if(c == '%'){
+        putc(buff, c, &index);
+      } else {
+        // Unknown % sequence.  Print it to draw attention.
+        putc(buff, '%', &index);
+        putc(buff, c, &index);
+      }
+      state = 0;
+    }
+  }
+}
+
 int 
 procfsisdir(struct inode *ip) {
 	if (!blockInit){
@@ -67,31 +131,24 @@ add_all(char *buff, int inum, int *offset){
 	add_one(buff, inum + 3, "ideinfo", offset);
 }
 
-
-int
-procfsread(struct inode *ip, char *dst, int off, int n) {
-	if (!blockInit){
-  		readsb(ip->dev, &block);
-  		blockInit = true;
-	}
-	if (IS_ALL(ip))
-	{
-		int offset = 0;
-		char buff[BUFFER_SIZE] = {0};
-		add_all(buff, block.ninodes, &offset);
-	}
-	/* code of IDEINFO def
-	Need To Put In File -
+void
+ideinfo_handler(char* buff){
+	/*  
+		code of IDEINFO def
+		Need To Put In File -
 		Waiting operations: <Number of waiting operations starting from idequeue>
 		Read waiting operations: <Number of read operations>
 		Write waiting operations: <Number of write operations>
 		Working blocks: <List (#device,#block) that are currently in the queue separated by the ‘;’
 		symbol> 
 	*/
-	if (IS_IDEINFO(ip))
-	{
+	sprintf(buff, "Waiting operations: %d\n", 1);
+}
 
-	}
+
+void
+filestat_handler(char* buff){
+
 	/* code of FILESTAT def
 		Need To Put In File -
 			Free fds: <free fd number (ref = 0)>
@@ -100,10 +157,11 @@ procfsread(struct inode *ip, char *dst, int off, int n) {
 			Readable fds: <Readable fd number>
 			Refs per fds: <ratio of total number of refs / number of used fds>
 	*/
-	if (IS_FILESTAT(ip))
-	{
-		
-	}
+}
+
+void
+inodeinfo_handler(char* buff){
+
 	/* code of INODEINFO def
 		Directory - Each File -
 			Name - index in the open inode table (in use)
@@ -116,10 +174,30 @@ procfsread(struct inode *ip, char *dst, int off, int n) {
 				hard links: <number of hardlinks>
 				blocks used: <number of blocks used in the file, 0 for DEV files>
 	*/
-	if (IS_INODEINFO(ip))
-	{
-		
+}
+
+int
+procfsread(struct inode *ip, char *dst, int off, int n) {
+	if (!blockInit){
+  		readsb(ip->dev, &block);
+  		blockInit = true;
 	}
+	if (IS_ALL(ip)) {
+		int offset = 0;
+		char buff[BUFFER_SIZE] = {0};
+		add_all(buff, block.ninodes, &offset);
+	}
+	char buff[100];
+
+	if (IS_IDEINFO(ip))
+		ideinfo_handler(buff);
+
+	if (IS_FILESTAT(ip))
+		filestat_handler(buff);
+
+	if (IS_INODEINFO(ip))
+		inodeinfo_handler(buff);
+
     return 0;
 }
 
