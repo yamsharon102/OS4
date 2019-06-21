265c47edacf2cf21e271413ce94eed1a811b43cd
Shlomi <shlomi9987@gmail.com>
2019-06-21 19:07:55 +0300

    Mamy stuff

--- procfs.c
+++ procfs.c
@@ -25,6 +25,17 @@ enum my_inum {PROC, IDEINFO, FILESTAT, INODEINFO};
 struct superblock block;
 int blockInit = 0;
 
+int get_free(struct file **files);
+int get_unique(struct file **files);
+int get_wr(struct file **files, int iswrite);
+int get_refs(struct file **files);
+
+char*
+get_buff(char* buff){
+	return buff + strlen(buff);
+}
+
+
 static void
 putc(char* buff, char c, int *index)
 {
@@ -32,7 +43,7 @@ putc(char* buff, char c, int *index)
 }
 
 static void
-sprinti(char *buff, int x, int *index)
+sprinti_helper(char *buff, int x, int *index)
 {
   char buf[16];
   int i = 0;
@@ -44,6 +55,11 @@ sprinti(char *buff, int x, int *index)
 
   while(--i >= 0)
     putc(buff, buf[i], index);
+}
+
+void sprinti(char *buff, int x){
+	int index = 0;
+	sprinti_helper(buff, x, &index);
 }
 
 void
@@ -66,7 +82,7 @@ sprintf(char* buff, char *fmt, ...)
       }
     } else if(state == '%'){
       if(c == 'd'){
-        sprinti(buff, *ap, &index);
+        sprinti_helper(buff, *ap, &index);
         ap++;
       } else if(c == 's'){
         s = (char*)*ap;
@@ -107,73 +123,73 @@ procfsiread(struct inode* dp, struct inode *ip) {
 	ip->minor = my_minor++; //???
 }
 
-void //check src type
-copy_to_buff(char *buff, void *src, int *offset){
-	int sizeToAdd = sizeof(src);
-	memmove(buff + *offset, src, sizeToAdd);
-	*offset += sizeToAdd;
-}
-
 void
-add_one(char *buff, int inum, char *dir, int *offset){
+add_one(char *buff, int inum, const char *dir){
 	struct dirent dirent;
 	dirent.inum = inum;
 	memmove(&dirent.name, dir, strlen(dir) + 1);
-	copy_to_buff(buff, (void *)&dirent, offset);
+	sprintf(get_buff(buff), (char*) &dirent);
 }
 
 void
-add_all(char *buff, int inum, int *offset){
-	add_one(buff, namei("/proc")->inum, ".", offset);
-	add_one(buff, namei("")->inum, "..", offset);
-	add_one(buff, inum + 1, "ideinfo", offset);
-	add_one(buff, inum + 2, "filestat", offset);
-	add_one(buff, inum + 3, "ideinfo", offset);
+add_all(char *buff, int inum){
+	add_one(buff, namei("/proc")->inum, ".");
+	add_one(buff, namei("")->inum, "..");
+	add_one(buff, inum + 1, "ideinfo");
+	add_one(buff, inum + 2, "filestat");
+	add_one(buff, inum + 3, "ideinfo");
 }
 
 void
 ideinfo_handler(char* buff){
-	/*  
-		code of IDEINFO def
-		Need To Put In File -
-		Waiting operations: <Number of waiting operations starting from idequeue>
-		Read waiting operations: <Number of read operations>
-		Write waiting operations: <Number of write operations>
-		Working blocks: <List (#device,#block) that are currently in the queue separated by the ‘;’
-		symbol> 
-	*/
-	sprintf(buff, "Waiting operations: %d\n", 1);
-}
+	sprintf(get_buff(buff), "%s:%d\n%s:%d\n%s:%d\n%s:",
+					"Waiting operations", get_waiting(),
+				    "Read waiting operations", get_read(),
+				    "Write waiting operations", get_write(),
+				    "Working blocks");
 
+	wblock *blocks[BSIZE];
+	get_working(blocks);
+
+	for (int i = 0; i < BSIZE; i++){
+		wblock *curr = blocks[i];
+		if(!curr)
+			break;
+
+		sprintf(get_buff(buff), "(%d,%d);", curr->device, curr->block);
+	}
+
+	sprintf(get_buff(buff), "\n");
+}
 
 void
 filestat_handler(char* buff){
+	struct file *files[NOFILE];
+	get_proc_files(files);
 
-	/* code of FILESTAT def
-		Need To Put In File -
-			Free fds: <free fd number (ref = 0)>
-			Unique inode fds: <Number of different inodes open by all the fds>
-			Writeable fds: <Writable fd number>
-			Readable fds: <Readable fd number>
-			Refs per fds: <ratio of total number of refs / number of used fds>
-	*/
+	sprintf(get_buff(buff), "%s:%d\n%s:%d\n%s:%d\n%s:%d\n%s:%d\n",
+		"Free fds", get_free(files),
+		"Unique inode fds", get_unique(files),
+		"Writable fds", get_wr(files, 1),
+		"Readable fds", get_wr(files, 0),
+		"Refs per fds", get_refs(files));
 }
 
 void
-inodeinfo_handler(char* buff){
+inodeinfo_handler(char* buff, int inum){
 
-	/* code of INODEINFO def
-		Directory - Each File -
-			Name - index in the open inode table (in use)
-			Content - 
-				Device: <device the inode belongs to>
-				Inode number: <inode number in the device>
-				is valid: <0 for no, 1 for yes>
-				type: <DIR, FILE or DEV>
-				major minor: <(major number, minor number)>
-				hard links: <number of hardlinks>
-				blocks used: <number of blocks used in the file, 0 for DEV files>
-	*/
+	struct inode *ip = get_inode(inum);
+  	if(!ip)
+  		return;
+
+	sprintf(get_buff(buff), "%s:%d\n%s:%d\n%s:%d\n%s:%d\n%s:(%d,%d)\n",
+		"Device", ip->dev,
+		"Inode number",ip->inum,
+		"is valid",ip->valid,
+		"type",ip->type == T_DEV ? "DEV" : ip->type == T_DIR ? "DIR" : "FILE",
+		"major minor",ip->major, ip->minor,
+		"hard links", ip->nlink, 
+		"blocks used", ip->type == T_DEV ? 0 : ip->size / 128);
 }
 
 int
@@ -182,23 +198,50 @@ procfsread(struct inode *ip, char *dst, int off, int n) {
   		readsb(ip->dev, &block);
   		blockInit = true;
 	}
-	if (IS_ALL(ip)) {
-		int offset = 0;
-		char buff[BUFFER_SIZE] = {0};
-		add_all(buff, block.ninodes, &offset);
-	}
-	char buff[100];
 
+	char buff[BSIZE * 10];
+	memset(buff, 0, BSIZE * 10);
+
+	if (IS_ALL(ip))
+		add_all(buff, block.ninodes);
+
+	/*  code of IDEINFO def
+		Need To Put In File -
+		Waiting operations: <Number of waiting operations starting from idequeue>
+		Read waiting operations: <Number of read operations>
+		Write waiting operations: <Number of write operations>
+		Working blocks: <List (#device,#block) that are currently in the queue separated by the ‘;’
+		symbol> */
 	if (IS_IDEINFO(ip))
 		ideinfo_handler(buff);
 
+	/* code of FILESTAT def
+		Need To Put In File -
+			Free fds: <free fd number (ref = 0)>
+			Unique inode fds: <Number of different inodes open by all the fds>
+			Writeable fds: <Writable fd number>
+			Readable fds: <Readable fd number>
+			Refs per fds: <ratio of total number of refs / number of used fds>*/
 	if (IS_FILESTAT(ip))
 		filestat_handler(buff);
 
+	/* code of INODEINFO def
+		Directory - Each File -
+		Name - index in the open inode table (in use)
+		Content - 
+		Device: <device the inode belongs to>
+		Inode number: <inode number in the device>
+		is valid: <0 for no, 1 for yes>
+		type: <DIR, FILE or DEV>
+		major minor: <(major number, minor number)>
+		hard links: <number of hardlinks>
+		blocks used: <number of blocks used in the file, 0 for DEV files>*/
 	if (IS_INODEINFO(ip))
-		inodeinfo_handler(buff);
+		inodeinfo_handler(buff, ip->inum);
 
-    return 0;
+	sprintf(dst, buff);
+
+    return strlen(buff);
 }
 
 int
@@ -215,4 +258,41 @@ procfsinit(void)
   devsw[PROCFS].iread = procfsiread;
   devsw[PROCFS].write = procfswrite;
   devsw[PROCFS].read = procfsread;
+}
+
+
+int
+get_free(struct file **files){
+	int ret = 0;
+    for(int i = 0; i < NOFILE; i++)
+        if(!files[i]->ref)
+            ret++;
+    return ret;
+} 
+
+int
+get_unique(struct file **files){
+	return 0;
+} 
+
+int
+get_wr(struct file** files, int iswrite){
+	int ret = 0;
+    for(int i = 0; i < NOFILE; i++){
+    	struct file *curr = files[i];
+        if(iswrite ? curr->writable : curr->readable)
+            ret++;
+    }
+    return ret;
+}
+
+int
+get_refs(struct file **files){
+	int ref, ret = 0, i = 0, empties = 0;
+    for(; i < NOFILE; i++){
+        ret += (ref = files[i]->ref);
+        if(!ref)
+        	empties++;
+    }
+    return ret / (i - empties);
 }
